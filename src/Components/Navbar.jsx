3dfdf176437b7b653eaf2cff8c0e778c829eb253
KaziSamiUllah <samiullahsagor@gmail.com>
2024-07-17 20:59:34 +0600

    login logout function

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,34 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
 
 const Navbar = () => {
+  const { user, SignOut } = useContext(AuthContext);
+  const changeLogin = () => {
+    SignOut();
+  };
+
   return (
     <div>
       <div className="navbar h-16 bg-primary flex justify-between md:px-8 lg:px-14">
-        <Link to="/"><h1 className="btn btn-ghost text-2xl font-bold drop-shadow-lg">EasyPAY</h1></Link>
-        <Link to="/login"><button className="btn bg-secondary border-none ">Login</button></Link>
+        <Link to="/">
+          <h1 className="btn btn-ghost text-2xl font-bold drop-shadow-lg">
+            EasyPAY
+          </h1>
+        </Link>
+        {user ? (
+          <button
+            onClick={changeLogin}
+            className="btn bg-secondary border-none "
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to="/login">
+            <button className="btn bg-secondary border-none ">Login</button>
+          </Link>
+        )}
       </div>
-     
-        
-      
     </div>
   );
 };
